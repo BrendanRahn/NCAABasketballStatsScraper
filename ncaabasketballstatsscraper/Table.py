a1b02b86d1b88815018067877803cd11795464b0
Brendan Rahn <brendanrahn01@gmail.com>
2025-05-21 03:37:23 -0500

    add init function to Table class to init some properties

--- ncaabasketballstatsscraper/Table.py
+++ ncaabasketballstatsscraper/Table.py
@@ -1,10 +1,20 @@
 from dataclasses import dataclass
+from ncaabasketballstatsscraper import CONSTS
 
 class Table:
+    schemaName: str
     tableName: str
     columns: list
     data: list = []
 
+    def __init__(self, tableName) -> None:
+        self.tableName = tableName
+        self.schemaName = tableName.split("_")[0]
+
+        if self.schemaName == "player":
+            self.columns = CONSTS.PLAYER_COLUMNS
+        elif self.schemaName == "team":
+            self.columns = CONSTS.TEAM_COLUMNS
 
     def appendData(self, data) -> None:
         self.data += data
